972debbe83836aacba9f04f4e46606c6fa040755
DenisKravchenko <denis.kravchenko@havi.com>
2023-04-08 22:27:11 +0300

    [task-3]: integrate with API

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,4 +1,4 @@
 import { setupWorker } from "msw";
 import { handlers } from "./handlers";
-// This configures a Service Worker with the given request handlers.
+// This configures a Service Worker with the given request product-service-handlers.
 export const worker = setupWorker(...handlers);
--- src/queries/products.ts
+++ src/queries/products.ts
@@ -8,9 +8,7 @@ export function useAvailableProducts() {
   return useQuery<AvailableProduct[], AxiosError>(
     "available-products",
     async () => {
-      const res = await axios.get<AvailableProduct[]>(
-        `${API_PATHS.bff}/product/available`
-      );
+      const res = await axios.get<AvailableProduct[]>(`${API_PATHS.product}`);
       return res.data;
     }
   );
@@ -29,7 +27,7 @@ export function useAvailableProduct(id?: string) {
     ["product", { id }],
     async () => {
       const res = await axios.get<AvailableProduct>(
-        `${API_PATHS.bff}/product/${id}`
+        `${API_PATHS.product}/${id}`
       );
       return res.data;
     },
@@ -58,7 +56,7 @@ export function useUpsertAvailableProduct() {
 
 export function useDeleteAvailableProduct() {
   return useMutation((id: string) =>
-    axios.delete(`${API_PATHS.bff}/product/${id}`, {
+    axios.delete(`${API_PATHS.product}/${id}`, {
       headers: {
         Authorization: `Basic ${localStorage.getItem("authorization_token")}`,
       },
