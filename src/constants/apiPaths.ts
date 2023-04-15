38d57ccfc4ef070ad578819425b846d2f7f5745a
DenisKravchenko <denis.kravchenko@havi.com>
2023-04-15 16:45:59 +0300

    [task-5]: integrate with s3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 const API_PATHS = {
   product:
-    "https://ya4eifso87.execute-api.us-east-1.amazonaws.com/dev/products",
-  order: "https://ya4eifso87.execute-api.us-east-1.amazonaws.com/dev",
-  import: "https://ya4eifso87.execute-api.us-east-1.amazonaws.com/dev",
-  bff: "https://ya4eifso87.execute-api.us-east-1.amazonaws.com/dev",
-  cart: "https://ya4eifso87.execute-api.us-east-1.amazonaws.com/dev",
+    "https://s6lbuxrm01.execute-api.us-east-1.amazonaws.com/dev/products",
+  order: "https://s6lbuxrm01.execute-api.us-east-1.amazonaws.com/dev",
+  import: "https://s6lbuxrm01.execute-api.us-east-1.amazonaws.com/dev",
+  bff: "https://s6lbuxrm01.execute-api.us-east-1.amazonaws.com/dev",
+  cart: "https://s6lbuxrm01.execute-api.us-east-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
